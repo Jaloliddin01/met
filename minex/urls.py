7cd8e1dd8cf3c6368bdf84fdc162b285609415cc
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-04-11 21:45:39 +0500

    added templates

--- minex/urls.py
+++ minex/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView, UserLogoutView
+from .views import UserRegistrationView, ExpenseView, HomeView, CategoryView, AboutView
 
 urlpatterns = [
-    path("registration/", UserRegistrationView.as_view()),
-    path("login/", UserLoginView.as_view()),
-    path("logout/", UserLogoutView.as_view()),
+    path("", HomeView.as_view(), name='home'),
+    path("registration/", UserRegistrationView.as_view(), name='registration'),
+    path("expenses/", ExpenseView.as_view(), name='expenses'),
+    path("categories/", CategoryView.as_view(), name='category'),
+    path("about/", AboutView.as_view(), name='about'),
 ]
--- minex/views.py
+++ minex/views.py
@@ -1,30 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpRequest, JsonResponse
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from .models import User
 
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
 class UserRegistrationView(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        if username and password:
-            user = User.objects.create_user(username=username, password=password)
-            user.save()
-            return JsonResponse({'status': 'user created successfully'})
-        return JsonResponse({'status': 'failed'})
+    def get(self, request):
+        return render(request, 'registration.html', {'status': 'registered successfully'})
 
-class UserLoginView(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return JsonResponse({'status': 'logged in successfully'})
-        return JsonResponse({'status': 'login failed'})
+class ExpenseView(View):
+    def get(self, request):
+        return render(request, 'expenses.html')
 
-class UserLogoutView(View):
-    def post(self, request: HttpRequest) -> JsonResponse:
-        logout(request)
-        return JsonResponse({'status': 'logged out successfully'})
+class CategoryView(View):
+    def get(self, request):
+        return render(request, 'category.html')
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'about.html')
+    
+
+# class UserLoginView(View):
+#     def post(self, request: HttpRequest) -> JsonResponse:
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user:
+#             login(request, user)
+#             return JsonResponse({'status': 'logged in successfully'})
+#         return JsonResponse({'status': 'login failed'})
+
+# class UserLogoutView(View):
+#     def post(self, request: HttpRequest) -> JsonResponse:
+#         logout(request)
+#         return JsonResponse({'status': 'logged out successfully'})
